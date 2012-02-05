b73aefec9dabba35b2b5d6dcddc694cbab4694a3
Ewan Andreasen <ea@vejlebib.dk>
2012-02-05 23:36:01 +0100

    rewrite as a feature module

--- views-view-field--redia-bapp--feed--field-list-image-fid.tpl.php
+++ views-view-field--redia-bapp--feed--field-list-image-fid.tpl.php
@@ -1,6 +1,6 @@
 <?php
- 
+// views-view-field--redia-bapp--feed--field-list-image-fid.tpl.php 
 ?>
   <redia-rss:item>
-    <redia-rss:url><?php print $output; ?></redia-rss:url> 
+    <redia-rss:url><?php print $output; ?></redia-rss:url>
   </redia-rss:item>
--- views-view-field--redia-bapp--feed--path.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php
- 
-?>
-  <redia-rss:item>
-    <redia-rss:url><?php print $output; ?></redia-rss:url> 
-  </redia-rss:item>
