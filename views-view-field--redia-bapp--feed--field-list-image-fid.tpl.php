06b28082c9f290d85b786998126790a051524a20
Ewan Andreasen <ea@vejlebib.dk>
2012-06-01 11:04:55 +0200

    Changed event end time to be event start time +1 hour. Works, since redia app doesn't display end times for events in the app.

--- views-view-field--redia-bapp--feed--field-list-image-fid.tpl.php
+++ views-view-field--redia-bapp--feed--field-list-image-fid.tpl.php
@@ -2,5 +2,5 @@
 // views-view-field--redia-bapp--feed--field-list-image-fid.tpl.php 
 ?>
   <redia-rss:item>
-    <redia-rss:url><?php print $output; ?></redia-rss:url>
+    <redia-rss:url><?php print $output; ?><?php dpm(get_defined_vars()); ?></redia-rss:url>
   </redia-rss:item>
---
+++ views-view-field--redia-bapp--feed--tid.tpl.php
@@ -0,0 +1,4 @@
+<?php
+// views-view-field--redia-bapp--feed--tid.tpl.php 
+?>
+<?php print $output; print '-xYz'; ?>
