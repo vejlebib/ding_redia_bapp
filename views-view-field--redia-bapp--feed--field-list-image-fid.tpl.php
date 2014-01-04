a9de40bc08aec719636f1a2976bbbcec9d09d3d0
wiredloose <ea@vejlebib.dk>
2014-01-04 12:49:17 +0100

    initial commit of 7.x version

--- views-view-field--redia-bapp--feed--field-list-image-fid.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php
-// views-view-field--redia-bapp--feed--field-list-image-fid.tpl.php 
-?>
-  <redia-rss:item>
-    <redia-rss:url><?php print $output; ?><?php dpm(get_defined_vars()); ?></redia-rss:url>
-  </redia-rss:item>
--- views-view-field--redia-bapp--feed--tid.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php
-// views-view-field--redia-bapp--feed--tid.tpl.php 
-?>
-<?php print $output; print '-xYz'; ?>
